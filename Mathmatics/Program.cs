a215b447eaabf2518626ee8ae08996beac357d34
joyannavb <joyannavb@gmail.com>
2022-05-05 12:08:09 -0500

    Mathbot created

---
+++ Mathmatics/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmatics
+{
+    internal class Program
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Multi(int num4, int num5)
+        {
+            return num4 * num5;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to mathbot! let's do some math!");
+            Console.WriteLine(" \r\n Give me a numer.");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Give me another number.");
+            int num2 = int.Parse(Console.ReadLine());
+            int sum = Add(num1,num2);
+            Console.WriteLine($"Mathbot says those two numbers add up to equal {sum}!");
+
+            Console.WriteLine("Let's do another one! Give me another number.");
+            int num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Give me one more number!");
+            int num4 = int.Parse(Console.ReadLine());
+            int multi = Multi(num3, num4);
+            Console.WriteLine($"Mathbot says when you multiply those numbers it equals {multi}.");
+
+
+        }
+    }
+}
